00cef3c6dd26955d7d6dcd91926a1c02d337f849
DonSutherland95 <58916345+DonSutherland95@users.noreply.github.com>
2023-06-26 11:07:58 -0400

    add blog functionality

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useContext } from "react";
 import Topbar from "./components/topbar/Topbar";
 import Single from "./pages/single/Single";
 import Write from "./pages/write/Write";
@@ -7,26 +7,21 @@ import Login from "./pages/login/Login";
 import Register from "./pages/register/Register";
 import Settings from "./pages/settings/Settings";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Context } from "./context/Context";
 
 function App() {
-  const currentUser = false;
+  const { user } = useContext(Context);
   return (
     <Router>
       <Topbar />
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route path="/posts" element={<Home />} />
-        <Route
-          path="/register"
-          element={currentUser ? <Home /> : <Register />}
-        />
-        <Route path="/login" element={currentUser ? <Write /> : <Login />} />
+        <Route path="/register" element={user ? <Home /> : <Register />} />
+        <Route path="/login" element={user ? <Write /> : <Login />} />
         <Route path="/post/:id" element={<Single />} />
-        <Route path="/write" element={currentUser ? <Write /> : <Login />} />
-        <Route
-          path="/settings"
-          element={currentUser ? <Settings /> : <Login />}
-        />
+        <Route path="/write" element={user ? <Write /> : <Login />} />
+        <Route path="/settings" element={user ? <Settings /> : <Login />} />
       </Routes>
     </Router>
   );
--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import React from "react";
-import ReactDOM from "react-dom";
+// import ReactDOM from "react-dom";
 import App from "./App";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { ContextProvider } from "./context/Context";
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(
+  <StrictMode>
+    <ContextProvider>
+      <App />
+    </ContextProvider>
+  </StrictMode>
+  // document.getElementById("root")
 );
